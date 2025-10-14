7b2653575c88f453099542b85f6462125b8518c3
han-son-03 <rkdgksths11@naver.com>
2025-10-14 10:51:27 +0900

    서버 연결 오류 발생, .env 파일 경로 못 찾음

--- config/settings/base.py
+++ config/settings/base.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
 from pathlib import Path
-
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0#)u0k)faywluws=l2s*n#chspi97acwlzy9xx)!)6&3sqx5tz'
+
+SECRET_KEY =env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -43,7 +43,6 @@ own_apps = []
 THIRD_PARTY_APPS = [
     'rest_framework',
     'drf_spectacular',
-    'SPECTACULAR_SETTINGS',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + own_apps + THIRD_PARTY_APPS
--- config/settings/dev.py
+++ config/settings/dev.py
@@ -5,10 +5,3 @@ from .base import *
 DEBUG = True
 ALLOWED_HOSTS = ['*'] # 개발 환경에서는 모든 호스트 허용
 
-# 2. 데이터베이스
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
