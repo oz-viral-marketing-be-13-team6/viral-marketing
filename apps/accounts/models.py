9ffce85926683f1498a33fe57a239c14be002c75
UNGBI78 <mybluewings78@gmail.com>
2025-10-15 16:02:00 +0900

    models.py 유저 클래스 수정( TextChoices로 통일 )

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,26 +1,30 @@
 from django.conf import settings
 from django.db import models
 from .bank_code_choices import BankCode
-from django_enum import EnumField
 
 class Users(models.Model):
-  class Role(models.TextChoices):
-    ADMIN = 'ADMIN', '관리자'
-    USER = 'USER', '사용자'
-    GUEST = 'GUEST', '손님'
+    class Role(models.TextChoices):
+        ADMIN = 'ADMIN', '관리자'
+        USER = 'USER', '사용자'
+        GUEST = 'GUEST', '손님'
 
-  user_id = models.AutoField(primary_key=True)
-  name = models.CharField(max_length=20)
-  password = models.CharField(max_length=50)
-  nickname = models.CharField(max_length=50, blank=True, null=True)
-  role = EnumField(Role, default=Role.USER)
-  last_login = models.DateTimeField()
-  created_at = models.DateTimeField(blank=True, null=True)
-  deleted_at = models.DateTimeField(blank=True, null=True)
+    user_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+    password = models.CharField(max_length=50)
+    nickname = models.CharField(max_length=50, blank=True, null=True)
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.USER,
+    )
+    last_login = models.DateTimeField()
+    created_at = models.DateTimeField(blank=True, null=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
 
-  class Meta:
-    managed = False
-    db_table = 'users'
+    class Meta:
+        managed = False
+        db_table = 'users'
+
 
 class Accounts(models.Model):
     account_id = models.AutoField(primary_key=True)
