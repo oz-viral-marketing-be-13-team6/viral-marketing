4d2fc705513f57ea63106110c3303731615b2eaf
Jun <dlwnsehd13@gmail.com>
2025-10-16 16:29:31 +0900

    ✅ Member_information
    작성

--- config/urls.py
+++ config/urls.py
@@ -15,19 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-# API Schema 파일 다운로드 경로
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    # Swagger UI (문서 보기) 경로
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+		path('admin/', admin.site.urls),
+		# API Schema 파일 다운로드 경로
+		path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+		# Swagger UI (문서 보기) 경로
+		path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 
-    #jwt
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+		# jwt
+		path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+		path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+		path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+		path('api/accounts/', include('apps.accounts.urls')),
 ]
