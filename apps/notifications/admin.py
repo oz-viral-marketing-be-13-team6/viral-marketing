a1a37fa45a90aebdb9a3ef779a40a90895d29aae
UNGBI78 <mybluewings78@gmail.com>
2025-10-20 19:01:33 +0900

    서버 배포전 임시 커밋

--- apps/notifications/admin.py
+++ apps/notifications/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Notification
 
-# Register your models here.
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'type', 'channel', 'is_read', 'created_at')
+    list_filter = ('type', 'channel', 'is_read')
+    search_fields = ('name', 'message')
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Users
 
-# Register your models here.
+@admin.register(Users)
+class UsersAdmin(admin.ModelAdmin):
+    # 검색 기능 (이메일, 닉네임)
+    search_fields = ('email', 'nickname')
+
+    # 필터링 (is_staff, is_active)
+    list_filter = ('is_staff', 'is_active')
+
+    # 관리자 목록에 표시될 필드
+    list_display = ('email', 'nickname', 'is_staff', 'is_active')
+
+    # 어드민 여부를 읽기 전용으로
+    readonly_fields = ('is_staff',)
